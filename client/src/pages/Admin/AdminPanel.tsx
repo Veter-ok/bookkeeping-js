f49a708873530fe0000643c927d82fe08c7df759
Veter-ok <61391385+Veter-ok@users.noreply.github.com>
2023-01-12 22:36:07 +0300

    Fix bugs. Add admin tool bar

--- client/src/pages/Admin/AdminPanel.tsx
+++ client/src/pages/Admin/AdminPanel.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import './AdminPanel.scss'
 import { Container } from "components/Containers/container"
-import { ButtonSubmit } from "components/ui/Buttons/button"
+import { Button, ButtonSubmit } from "components/ui/Buttons/button"
 import { Input, InputDate } from "components/ui/Input/input"
 import { Select } from "components/ui/Select/select"
 import React, {FunctionComponent as FC, FormEvent, useEffect, useState} from "react"
@@ -56,24 +56,34 @@ const AdminPanel:FC = () => {
 	}
 
 	return (
-		<div className="admin_panel-container-group">
-			<Container id="container-1" title="Добавить пользователя">
-				<form onSubmit={addUser}>
-					<Input placeholder="Имя" type="text" value={name} onChange={setName}/>
-					<Input placeholder="Фамилия" type="text" value={surname} onChange={setSurname}/>
-					<Select options={roles} onChange={setRole}/>
-					<Input placeholder="Email: example@gmail.com" type="text" value={email} onChange={setEmail}/>
-					<Input placeholder="Пароль" type="text" value={password} onChange={setPassword}/>
-					<InputDate value={date} onChange={setDate}/>
-					<ButtonSubmit text="Добавить"/>
-				</form>
-			</Container>
-			<Container id="container-2" title="Пользователи">
-				{users.map((user) => 
-					<UserBlock key={user.id} user={user} del={deleteUser}/>
-				)}
-			</Container>
-		</div>
+		<>
+			<div className="switch-windows">
+				<ul className="switch-windows__list-windows">
+					<li className="">Пользователи</li>
+					<li className="">Банки</li>
+					<li className="">Карты</li>
+					<li className="">Счета</li>
+				</ul>
+			</div>
+			<div className="admin_panel-container-group">
+				<Container id="container-1" title="Добавить пользователя">
+					<form onSubmit={addUser}>
+						<Input placeholder="Имя" type="text" value={name} onChange={setName}/>
+						<Input placeholder="Фамилия" type="text" value={surname} onChange={setSurname}/>
+						<Select options={roles} onChange={setRole}/>
+						<Input placeholder="Email: example@gmail.com" type="text" value={email} onChange={setEmail}/>
+						<Input placeholder="Пароль" type="text" value={password} onChange={setPassword}/>
+						<InputDate value={date} onChange={setDate}/>
+						<ButtonSubmit text="Добавить"/>
+					</form>
+				</Container>
+				<Container id="container-2" title="Пользователи">
+					{users.map((user) => 
+						<UserBlock key={user.id} user={user} del={deleteUser}/>
+					)}
+				</Container>
+			</div>
+		</>
 	)
 }
 
